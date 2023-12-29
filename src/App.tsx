b06eedb32f736a6c2ef817ad527c6cab9f50a87b
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2023-12-29 16:24:07 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
 export function App() {
- return(<h1>SETUP</h1>)
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button>Enviar</Button>
+    </div>
+  )
 }
