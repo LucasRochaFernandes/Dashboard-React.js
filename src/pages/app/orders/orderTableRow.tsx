16fec02d82c6da64c18fd2e5c2413b9e8f27438d
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2024-01-02 13:39:16 -0300

    Dialog

---
+++ src/pages/app/orders/orderTableRow.tsx
@@ -0,0 +1,50 @@
+import { DialogContent } from '@radix-ui/react-dialog'
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+import { OrderDetails } from './orderDetails'
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+          <OrderDetails />
+        </Dialog>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        fdsfsdfsdfsdfsdfsdfsdfsf
+      </TableCell>
+      <TableCell className="text-muted-foreground">Há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Diefo gernandes</TableCell>
+      <TableCell className="font-medium">R$ 140,90</TableCell>
+      <TableCell className="">
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-3 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-3 h-3 w-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
