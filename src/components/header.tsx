3bc206b9518791f83757e0f3af32fb4fc446bfe6
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2024-01-02 01:14:07 -0300

    Account Menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,8 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 
+import { AccountMenu } from './account-menu'
 import { NavLink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
 import { Separator } from './ui/separator'
 
 export function Header() {
@@ -10,15 +12,19 @@ export function Header() {
         <Pizza className="h-6 w-6" />
         <Separator orientation="vertical" className="h-6" />
         <nav className="flex items-center space-x-4 lg:space-x-6">
-          <NavLink to="/orders">
+          <NavLink to="/">
             <Home className="h-4 w-4" />
             Início
           </NavLink>
-          <NavLink to="/">
+          <NavLink to="/orders">
             <UtensilsCrossed className="h-4 w-4" />
             Pedidos
           </NavLink>
         </nav>
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
       </div>
     </div>
   )
