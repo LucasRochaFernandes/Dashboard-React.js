89a530278fffe12f166283221c61009d23a6e1c0
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2024-01-01 22:23:22 -0300

    Sign Up

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -22,6 +23,7 @@ export default function SignIn() {
 
   async function handleSignIn(data: SignInForm) {
     try {
+      console.log(data.email)
       await new Promise((resolve) => setTimeout(resolve, 2000))
       toast.success('Enviamos um link de autenticação para seu e-mail.', {
         action: {
@@ -38,6 +40,10 @@ export default function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant={'ghost'} asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[320px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
