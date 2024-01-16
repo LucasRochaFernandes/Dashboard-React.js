67ddaf016974edc8cdb78d1eb65d3841dce5e9e6
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2024-01-16 12:52:26 -0300

    Add React Query

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -18,7 +18,6 @@ export function Orders() {
       <Helmet title="Pedidos" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-
         <div className="space-y-2.5">
           <OrderTableFilters />
           <div className="rounded-md border">
