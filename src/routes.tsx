3a5e1328d0333e3f9becdfc8a21ccb59b279a1f0
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2024-01-19 14:40:03 -0300

    Error Page

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,11 +7,12 @@ import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
+import { ErrorPage } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: '/',
@@ -35,5 +36,9 @@ export const router = createBrowserRouter([
         element: <SignUp />,
       },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
